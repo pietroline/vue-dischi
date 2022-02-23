c902a17cd8ce8e1ececcf616130a48947de318cf
pietroline <pietrobarberi@hotmail.it>
2022-02-23 18:05:45 +0100

    fix style card album

--- src/components/partials/SongCard.vue
+++ src/components/partials/SongCard.vue
@@ -1,14 +1,14 @@
 <template>
     <div id="contenitore">
         
-        <a href="#" class="card my-2">
-
+        <a href="#" class="card d-flex align-items-center my-2">
+            <!-- la classe card prevede display flex con flex-direction column -->
             <div class="d-flex justify-content-center">
                 <img class="w-75 py-3" :src="song.poster" :alt="song.ganze">
             </div>
             
             <div class="card-body text-center">
-                <h1 class="fs-5">{{song.title}}</h1>
+                <h1 class="fs-5 uppercase">{{song.title}}</h1>
                 <div class="text_grey">{{song.author}}</div>
                 <div class="text_grey">{{song.year}}</div>
             </div>
@@ -40,7 +40,13 @@
 
             height: 22rem;
 
-            text-transform: uppercase;
+            .card-body{
+                width: 90%;
+
+                .uppercase{
+                    text-transform: uppercase;
+                }
+            }
 
             .text_grey{
                 color: $greyText;
